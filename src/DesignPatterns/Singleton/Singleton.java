1a272065c022e20c1c4ef7d076645f79e764a959
farellfebriano <farellfebriano8@gmail.com>
2025-11-29 21:29:37 -0600

    singleton

---
+++ src/DesignPatterns/Singleton/Singleton.java
@@ -0,0 +1,20 @@
+package DesignPatterns.Singleton;
+
+class Singleton{
+    private static Singleton instance;
+    private String data;
+
+    private Singleton(String data){
+        this.data=data;
+
+    }
+    public static Singleton getInstance(String data){
+        //
+        synchronized (Singleton.class){
+            if (instance == null){
+                instance = new Singleton(data);
+            }
+            return instance;
+        }
+    }
+}
